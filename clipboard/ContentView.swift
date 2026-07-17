ca5ab898f166a6238d370e6d915cd6fc754e74ef
sameer-soni <tenyson2005@gmail.com>
2026-07-18 03:33:20 +0530

    Initial Commit

---
+++ clipboard/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  clipboard
+//
+//  Created by Sameer Soni on 18/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ clipboard/clipboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  clipboardApp.swift
+//  clipboard
+//
+//  Created by Sameer Soni on 18/07/26.
+//
+
+import SwiftUI
+
+@main
+struct clipboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
